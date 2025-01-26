98da5d0fbbb33aa07e39013dbfe1a92c7a168e6c
infi-defi <sot.ainatzis@gmail.com>
2025-01-26 19:19:20 +0200

    backup commit

---
+++ Day12/day12.py
@@ -0,0 +1,14 @@
+with open('day12.txt', 'r') as f:
+    grid = f.read().split()
+
+row_len = len(grid)
+col_len = len(grid[0])
+
+
+
+for r in range(row_len):
+    for c in range(col_len):
+
+
+while True:
+    
